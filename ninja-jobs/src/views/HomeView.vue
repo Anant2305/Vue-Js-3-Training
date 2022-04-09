114702274ac122e320eadd9177a3cb0123ec057f
Anant2305 <anant2305@github.com>
2022-04-09 11:22:27 +0100

    Ninja-jobs
    Routing

---
+++ ninja-jobs/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit harum quibusdam, deserunt enim ratione, provident corrupti sint ad eaque veritatis minima maxime quod atque similique ab quidem fugit asperiores earum. Veritatis ullam pariatur perferendis unde exercitationem voluptas molestiae ipsa quidem voluptatem harum recusandae doloribus dolorem, et omnis animi. Alias itaque tempora totam laborum tenetur modi quisquam, cumque labore quibusdam, dignissimos commodi corrupti aliquid explicabo repudiandae illo doloremque ut ab obcaecati fugit quaerat. Dolore, iure? Necessitatibus dolor quibusdam laudantium a totam, fugit cupiditate molestias quo optio excepturi at officia earum quam quod magnam dicta tenetur porro exercitationem praesentium sapiente aperiam! Sunt?</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit harum quibusdam, deserunt enim ratione, provident corrupti sint ad eaque veritatis minima maxime quod atque similique ab quidem fugit asperiores earum. Veritatis ullam pariatur perferendis unde exercitationem voluptas molestiae ipsa quidem voluptatem harum recusandae doloribus dolorem, et omnis animi. Alias itaque tempora totam laborum tenetur modi quisquam, cumque labore quibusdam, dignissimos commodi corrupti aliquid explicabo repudiandae illo doloremque ut ab obcaecati fugit quaerat. Dolore, iure? Necessitatibus dolor quibusdam laudantium a totam, fugit cupiditate molestias quo optio excepturi at officia earum quam quod magnam dicta tenetur porro exercitationem praesentium sapiente aperiam! Sunt?</p>
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'HomeView',
+  components: {
+  }
+}
+</script>
